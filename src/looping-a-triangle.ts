5df8205cdb44b2556ad769b9602ccb90e20fe4c6
Roberto Andrade <robert.andrade@zinnia.com>
2023-07-19 15:58:03 +0000

    add looping a triangle

---
+++ src/looping-a-triangle.ts
@@ -0,0 +1,32 @@
+/* 
+1. Looping A Triangle
+
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+
+It may be useful to know that you can find the length of a string by writing .length after it.
+
+    let abc = "abc";
+    console.log(abc.length);
+    // → 3
+*/
+
+/**
+ * Outputs a triangle of `#` characters to the console
+ * where the length of each line equals its 1-based index.
+ *
+ * @param {number} lines - The number of lines in the triangle.
+ */
+export const loopingATriangle = (lines: number = 7) => {
+  Array.from({ length: lines }, (_, i) => {
+    const str = '#'.repeat(i + 1);
+    console.log(str);
+  });
+}
---
+++ tests/looping-a-triangle.test.ts
@@ -0,0 +1,36 @@
+import { loopingATriangle } from '../src/looping-a-triangle'
+
+describe("loopingATriangle function", () => {
+  let spy: jest.SpyInstance;
+
+  beforeEach(() => {
+    // mock console.log to prevent output in terminal
+    spy = jest.spyOn(console, 'log').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    spy.mockRestore();
+  });
+
+  it("should be called 7 times", () => {
+    loopingATriangle();
+    expect(spy).toHaveBeenCalledTimes(7);
+  });
+
+  const triangle = [
+    '#',
+    '##',
+    '###',
+    '####',
+    '#####',
+    '######',
+    '#######'
+  ]
+
+  triangle.forEach((str, i) => {
+    it(`should be called with ${str}`, () => {
+      loopingATriangle();
+      expect(spy).toHaveBeenNthCalledWith(i + 1, str);
+    })
+  })
+})
