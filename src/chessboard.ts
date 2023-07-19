1609003a636eef08220be6c90f6b985758e4abd7
Roberto Andrade <robert.andrade@zinnia.com>
2023-07-19 21:44:03 +0000

    change special character

--- src/chessboard.ts
+++ src/chessboard.ts
@@ -1,6 +1,6 @@
 /**
  * Chessboard
-Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines.
+Write a program that creates a string that represents an 8x8 grid, using newline characters to separate lines.
 At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
 Passing this string to console.log should show something like this:
 
